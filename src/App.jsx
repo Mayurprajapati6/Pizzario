5cd894a9228e78aab01a721b5207bd380c72e0ab
Mayur Prajapati <mproyur685@gmail.com>
2025-06-26 23:19:28 +0530

    Added signForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route , Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
+import Signup from './Pages/Auth/Signup'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>  
+      <Route path="/auth/signup" element={<Signup />} />
     </Routes>
   )
 }
